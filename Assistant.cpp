47759eb9bd3696684af4dbae126966a4806c4fa3
Vedanta SP <vedant.vasu1111@gmail.com>
2020-07-06 09:31:24 +0530

    Update Assistant.cpp

--- Assistant.cpp
+++ Assistant.cpp
@@ -1,6 +1,10 @@
+// Assistant.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #include <iostream>
 #include <windows.h>
 #include <shellapi.h>
+#include <string>
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
@@ -15,43 +19,46 @@ void clrscr()
 // Function Prototypes
 void launchSpotify();     // Using Pascal method to name the functions
 void launchAccuWeather();
-
+void makeLIST();
 int main()
 {
+    using namespace std;
     // system("sety raw");
     clrscr(); // Clearing the screen every time main() in called
     char ch;
     cout << "\tHey \n\tI am your Assistant but I'm still learning so if you encounter any bugs , forgive me";
+
     cout << "\n\tHere is some stuff i can currently do ";
-    cout << "\n\t 1. Open Spotify (s)\n\t 2. Tell you the current weather (w)\n\t 3. Exit (q)" ;
-    cout << "\nWhat do you want me to do ? :";
-    ch = getchar();
-        switch (ch) {
-            case 's' :
+            cout << "\n\t 1. Open Spotify (s)\n\t 2. Tell you the current weather (w)\n\t3.Quit(q)\n\t";
+            cout << "\nWhat do you want me to do ? :";
+            ch = getchar();
+            switch (ch) {
+            case 's':
                 cout << "\n\t Launching Spotify on Web";
                 launchSpotify();
                 break;
-            case 'w' :
+            case 'w':
                 cout << "\n\t Launching AccuWeather on Web";
                 launchAccuWeather();
                 break;
-            case 'q' :
+            case 'q':
                 cout << "\n\t Exiting";
                 exit(1);
             default: cout << "\n\t Invalid input, please try again";
-                     main(); // This is a recursive call
+                main(); // This is a recursive call
+            }
         }
+    };
+
+
+    void launchSpotify() {
+        clrscr();
+        ShellExecute(0, 0, "https://open.spotify.com/", 0, 0, SW_SHOW);
+        main(); // Going back to the main() function
     }
 
-
-void launchSpotify() {
-    clrscr();
-    ShellExecute(0, 0, "https://open.spotify.com/", 0, 0, SW_SHOW);
-    main(); // Going back to the main() function
-}
-
-void launchAccuWeather() {
-    clrscr();
-    ShellExecute(0, 0, "https://www.accuweather.com/", 0, 0, SW_SHOW);
-    main(); // Going back to the main() function
-}
+    void launchAccuWeather() {
+        clrscr();
+        ShellExecute(0, 0, "https://www.accuweather.com/", 0, 0, SW_SHOW);
+        main(); // Going back to the main() function
+    }
